19d0c4d93868be21d98b8ad2f305dde7a0c93181
Wudson Melo <wudson@sistemasbr.net>
2023-04-13 20:40:31 -0300

    adicionando flatlist e funções para adicionar e remover itens

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,31 +1,91 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList, Button } from "react-native";
 import { styles } from "./styles";
+import React, { useState } from "react";
 
 export function Home() {
+
+  const [tarefas, setTarefas] = useState<string[]>([]);
+  const [tarefa, setTarefa] = useState('');
+
+  function addTarefa() {
+    setTarefas(prev => [...prev, tarefa])
+  }
+
+  function removeTarefa(item: string) {
+    setTarefas(prev => prev.filter(tarefa => tarefa !== item))
+    console.log(tarefas);
+  }
+
   return (
     <>
       <View style={styles.topContainer}>
-        <View style={styles.container}>
-      <Image
-        style={styles.tinyLogo}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
-      />
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
         <Text style={styles.titulo}>todo</Text>
-        </View>
       </View>
+
       <View style={styles.bottomContainer}>
-        <Text>Titulo</Text>
+        <View style={styles.container}>
+          <View style={styles.coluna1}>
+            <Text style={styles.textoColuna1}>Criadas</Text>
+            <Text style={styles.contador}>0</Text>
+          </View>
+          <View style={styles.coluna2}>
+            <Text style={styles.textoColuna2}>Concluidas</Text>
+            <Text style={styles.contador}>0</Text>
+          </View>
+        </View>
+
+        <FlatList
+          data={tarefas}
+          renderItem={({ item }) => 
+          <View style={styles.cardTarefa}>
+            <View style={{flex: 1}}>
+          <Text style={styles.radio}></Text>
+          </View>
+          <Text style={styles.tarefa}>{item}</Text>
+          <TouchableOpacity 
+          style={styles.remover} 
+          onPress={() => removeTarefa(item)}>
+            <Text>
+              🗑
+            </Text>
+          </TouchableOpacity>
+          </View>
+        }
+          keyExtractor={item => item}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={() =>
+            <View style={styles.listaVazia}>
+              <Image
+
+                  source={require('../../../assets/favicon.png')}
+                    />
+              <Text style={styles.listaVazia1}>
+                Você ainda não tem tarefas cadastradas
+              </Text>
+              <Text style={styles.listaVazia2}>
+                Crie tarefas e organize seus itens a fazer
+              </Text>
+            </View>
+          }
+        />
       </View>
+
       <View style={styles.buttonContainer}>
         <View style={styles.linhaInput}>
           <TextInput
             style={styles.input}
             placeholder="Adicione uma nova tarefa"
-            placeholderTextColor='#333333' />
+            placeholderTextColor='#333333'
+            onChangeText={setTarefa} />
           <TouchableOpacity
-            style={styles.buttonAdd}>
+            style={styles.buttonAdd}
+            onPress={addTarefa}>
             <Text style={styles.textButtonAdd}>
               +
             </Text>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,26 +2,32 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
-        flex: 1,
         flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
+        borderColor: '#333333',
+        borderBottomWidth: 1,
+        paddingBottom: 20,
+        paddingTop: 60,
+        marginBottom: 20
     },
     bottomContainer: {
-        flex: 1,
-        backgroundColor: '#1A1A1A'
+        flex: 6,
+        backgroundColor: '#1A1A1A',
+        paddingHorizontal: 25
     },
     topContainer: {
+        flex: 2,
+        flexDirection: 'row',
         backgroundColor: '#0D0D0D',
-        height: '25%'
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     buttonContainer: {
         position: 'absolute',
         width: '100%',
-        height: '50%',
         justifyContent: 'center',
         alignItems: 'center',
-        flexDirection: "row"
+        flexDirection: "row",
+        height: '50%'
     },
     input: {
         flex: 1,
@@ -59,5 +65,66 @@ export const styles = StyleSheet.create({
         fontSize: 60,
         fontWeight: 'bold',
         marginLeft: 15
+    },
+    coluna1:{
+        flex: 1,
+        flexDirection: "row"
+    },
+    coluna2:{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: 'flex-end',
+    },
+    textoColuna1:{
+        marginRight: 10,
+        color: '#4184ad'
+    },
+    textoColuna2:{
+        marginRight: 10,
+        color: '#4e4f8a'
+    },
+    contador: {
+        backgroundColor: '#333333',
+        color: '#D9D9D9',
+        borderRadius: 20,
+        paddingHorizontal: 8
+    },
+    tarefa:{
+        flex: 9,
+        color: '#D9D9D9',
+        marginHorizontal: 10
+    },
+    listaVazia:{
+        flex: 1,
+        alignItems: 'center',
+        marginTop: 40
+    },
+    listaVazia1:{
+        color: '#808080',
+        fontWeight: 'bold'
+    },
+    listaVazia2:{
+        color: '#808080'
+    },
+    cardTarefa:{
+        flexDirection: 'row',
+        backgroundColor: '#262626',
+        alignItems: 'center',
+        height: 60,
+        borderRadius: 10,
+        marginBottom: 5,
+        paddingHorizontal: 10
+    },
+    radio:{
+        width: 25,
+        height: 25,
+        borderRadius: 100,
+        borderColor: '#4184ad',
+        borderWidth: 3
+    },
+    remover:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
